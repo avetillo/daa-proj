c6ef002c1861182a01f124fe8591ba1184664bf9
avegail <avegailtillo.pup@gmail.com>
2024-06-30 04:26:46 +0800

    daa-proj

---
+++ javascript/scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('start').addEventListener('click', function() {
+    window.location.href = 'leaflet.html';
+});
+
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('about').addEventListener('click', function() {
+    alert('About button clicked');
+});
+
+document.getElementById('simulate')?.addEventListener('click', function() {
+    alert('Simulate button clicked');
+});
+
+document.getElementById('details')?.addEventListener('click', function() {
+    alert('Details button clicked');
+});
